4c92a66798996ea4b52c10e28564424ae5250f1a
RickyCarrera <rickycarrera@csu.fullerton.edu>
2020-08-05 18:31:07 -0700

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TestRPG
+
+Developed with Unreal Engine 4
---
+++ Source/TestRPG/TestRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTRPG_API ATestRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
